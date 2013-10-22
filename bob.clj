1afdc1f3b2083ea09f7ce6a9a148cdc926736d41
David Gerber <gerbdla@gmail.com>
2013-10-22 10:50:12 -0700

    add comments

--- bob.clj
+++ bob.clj
@@ -1,20 +1,24 @@
 (ns bob
   (require [clojure.string :as str]))
 
-#has a letter or has a number
+(defn- silence?    [msg] (str/blank? msg))
 
-(defn- question? [msg] (= \? (last msg)))
+(defn- question?   [msg] (= \? (last msg)))
+
 (defn- has-letter? [msg] (some #(or (Character/isUpperCase %)
                                     (Character/isLowerCase %)) msg))
 
-(defn- shouting? [msg] (and (= msg (str/upper-case msg))
+(defn- shouting?   [msg] (and (= msg (str/upper-case msg))
                               (has-letter? msg)))
 
 (defn response-for [input]
   (cond
+    ;; is the string empty?
     (str/blank? input) "Fine. Be that way!"
-    (= input (str/upper-case input)) "Woah, chill out!"
-    (= some #(Character/isUpperCase %) input)
+    ;; is the string uppercase and has a letter.
+    (and (= input (str/upper-case input))(has-letter? input))  "Woah, chill out!"
+    ;; is the last input a question mark?
     (= \? (last input)) "Sure."
+    ;; everthing else falls to numbers
     :else "Whatever.")
     )
